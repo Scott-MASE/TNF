aca7e5e5209d8eb6d6cbfa2baecb7df93e4086e2
Scott-MASE <a00325403@student.tus.ie>
2025-06-02 11:41:17 +0100

    fixed issue with lombok in common preventing builds

--- theFellows/common/src/main/java/com/tus/common/entity/Anomaly.java
+++ theFellows/common/src/main/java/com/tus/common/entity/Anomaly.java
@@ -23,4 +23,53 @@ public class Anomaly {
     private Double trafficVolume;
     @Column(name = "datetime")
     private LocalDateTime timestamp;
+
+    public Long getId() {
+        return id;
+    }
+
+    public Integer getNodeId() {
+        return nodeId;
+    }
+
+    public Integer getNetworkId() {
+        return networkId;
+    }
+
+    public String getAnomalyType() {
+        return anomalyType;
+    }
+
+    public Double getTrafficVolume() {
+        return trafficVolume;
+    }
+
+    public LocalDateTime getTimestamp() {
+        return timestamp;
+    }
+
+    // Setters
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setNodeId(Integer nodeId) {
+        this.nodeId = nodeId;
+    }
+
+    public void setNetworkId(Integer networkId) {
+        this.networkId = networkId;
+    }
+
+    public void setAnomalyType(String anomalyType) {
+        this.anomalyType = anomalyType;
+    }
+
+    public void setTrafficVolume(Double trafficVolume) {
+        this.trafficVolume = trafficVolume;
+    }
+
+    public void setTimestamp(LocalDateTime timestamp) {
+        this.timestamp = timestamp;
+    }
 }
