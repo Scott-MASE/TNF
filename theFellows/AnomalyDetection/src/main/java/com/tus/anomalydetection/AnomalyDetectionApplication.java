ea6beae9db3ed61170714ff707b341d6f00ca8ad
Smitha Gujjaru <A00277349@student.tus.ie>
2025-06-03 18:50:47 +0100

    @EnableSheduling is been added here in this class

--- theFellows/AnomalyDetection/src/main/java/com/tus/anomalydetection/AnomalyDetectionApplication.java
+++ theFellows/AnomalyDetection/src/main/java/com/tus/anomalydetection/AnomalyDetectionApplication.java
@@ -3,9 +3,11 @@ package com.tus.anomalydetection;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EntityScan(basePackages = {"com.tus.common.entity"})
+@EnableScheduling
 public class AnomalyDetectionApplication {
 
     public static void main(String[] args) {
